785789d9b263478b06abe60c92314c143dc3975e
pahae-tests <wahipahae@gmail.com>
2026-02-19 11:37:04 +0000

    Update _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import Head from 'next/head'
 import styles from '../styles/globals.css'
 import Header from "@/components/Header"
 import { useRouter } from 'next/router';
@@ -13,10 +14,20 @@ export default function MyApp({ Component, pageProps }) {
     }, []);
 
     return (
-        <div className='w-full h-screen'>
-            {router.pathname !== "/" && <Header isDark={isDark} setIsDark={setIsDark} />}
-            <Component {...pageProps} isDark={isDark} />
-        </div>
-    );
+        <>
+            <Head>
+                <title>Nom de ton site</title>
+                <link rel="icon" href="/logo.png" />
+                <link rel="apple-touch-icon" href="/logo.png" />
+                <meta name="theme-color" content="#000000" />
+            </Head>
 
+            <div className='w-full h-screen'>
+                {router.pathname !== "/" && 
+                    <Header isDark={isDark} setIsDark={setIsDark} />
+                }
+                <Component {...pageProps} isDark={isDark} />
+            </div>
+        </>
+    );
 }
